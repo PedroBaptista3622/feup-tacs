1c5e35a47777689674066c07450b4621ebb36773
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2021-12-04 22:50:31 +0000

    code block css template

--- building-blocks/src/components/CodeComponent.tsx
+++ building-blocks/src/components/CodeComponent.tsx
@@ -1,12 +1,16 @@
 import "../styles/code.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const codeExample = "void teste(int number) {return number;}";
+const codeExample2 = "void teste2(int number) { return number; }";
+
 function CodeComponent(): JSX.Element {
   return (
     <div className="Code-Generated">
       <div className="codeHeader">Code</div>
       <div className="codeBody">
-        <textarea className="code"> </textarea>
+        <div className="codeSection">{codeExample}</div>
+        <div className="codeSection">{codeExample2}</div>
       </div>
     </div>
   );
